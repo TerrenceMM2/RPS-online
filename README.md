61547ba79355190fc0439f1f1c10eb8de09055b2
TerrenceMM2 <terrencemm2@gmail.com>
2019-06-04 14:49:23 -0500

    sets initial form and push/pull content from Firebase DB

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # coursework7-rps-multiplayer
-Vanderbilt Coding Boot Camp - Coursework7 - Rock, Paper, Scissors Multiplayer Game
+Vanderbilt Coding Boot Camp - Coursework7 - Rock, Paper, Scissors Multiplayer Game  
+Live Link: Coming Soon ...  
+  
+### Pseudocode  
+1. Initialize DB values  
+2. Player 1 enters unique username (<input>)  
+    2. If userName in DB, notify end-user  
+    2. Store name in DB  
+    2. Set Player 1 in DB  
+    2. Show name on page  
+3. Show Player 2 data (relative)  
+4. Show rock, paper, scissors option  
+5. Start time (5 seconds)  
+6. Users clicks options  
+    6. Store in DB  
+    6. Show P1 choice  
+    6. Show P2 choice  
+7. if/else statement  
+    7. Win  
+        7. Set Winner in DB  
+        7. winCount++ in DB  
+    7. Loss  
+        7. if not Winner  
+        7. lossCount++ in DB  
+    7. roundCount++  
+    7. Show Winner from DB  
+8. After 5 rounds, calculate winner  
---
+++ assets/js/app.js
@@ -0,0 +1,67 @@
+// Your web app's Firebase configuration
+var config = {
+    apiKey: "AIzaSyBLw--mS2bA2RRZXOi9nbadZavdBA1JaUA",
+    authDomain: "coursework7-rps.firebaseapp.com",
+    databaseURL: "https://coursework7-rps.firebaseio.com",
+    projectId: "coursework7-rps",
+    storageBucket: "",
+    messagingSenderId: "288062732004",
+    appId: "1:288062732004:web:832a923f7ff1acac"
+};
+// Initialize Firebase
+firebase.initializeApp(config)
+
+var database = firebase.database();
+
+// Initial Values
+var firstName = "";
+var lastName = "";
+var userName = "";
+var winRecord = 0;
+var lossRecord = 0;
+
+// Round Initial Values
+var roundCount = 0;
+var winCount = 0;
+var lossCount = 0;
+
+database.ref().on("value", function (snapshot) {
+
+    if (snapshot.child("username").exists()) {
+        // Set the variables for highBidder/highPrice equal to the stored values in firebase.
+        userName = snapshot.val().userName;
+        winRecord = snapshot.val().winRecord;
+        lossRecord = snapshot.val().lossRecord;
+
+        // Change the HTML to reflect the stored values
+        $("#username-display").text(userName);
+        $("#record-display").text(winRecord + " - " + lossRecord);
+    }
+});
+
+$("#set-player").on("click", function (event) {
+
+    event.preventDefault();
+
+    firstName = $("#first-name-input").val().trim();
+    lastName = $("#last-name-input").val().trim();
+    userName = $("#username-input").val().trim();
+
+    database.ref().set({
+        firstName: firstName,
+        lastName: lastName,
+        userName: userName,
+        winRecord: winRecord,
+        lossRecord: lossRecord
+    });
+
+    database.ref().on("value", function (snapshot) {
+        userName = snapshot.val().userName;
+        winRecord = snapshot.val().winRecord;
+        lossRecord = snapshot.val().lossRecord;
+    });
+
+    $("#username-display").text(userName);
+    $("#record-display").text(winRecord + " - " + lossRecord);
+
+});
