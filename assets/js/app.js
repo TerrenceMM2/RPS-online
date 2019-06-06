65d7fb4466548a23168b14a0ac50dc90b40f605f
TerrenceMM2 <terrencemm2@gmail.com>
2019-06-06 14:32:10 -0500

    sets player 1 and 2 objects in database

--- assets/js/app.js
+++ assets/js/app.js
@@ -25,6 +25,12 @@ var roundCount = 0;
 var winCount = 0;
 var lossCount = 0;
 
+var existingUserName = "";
+var existingUserObj;
+
+var playerOne;
+var playerTwo;
+
 database.ref().on("value", function (snapshot) {
 
     if (snapshot.child("username").exists()) {
@@ -47,55 +53,68 @@ $("#set-player").on("click", function (event) {
     lastName = $("#last-name-input").val().trim();
     userName = $("#username-input").val().trim();
 
+    // Sets the search reference location if an existing username is found
+    var userNameSearch = database.ref("/users").orderByChild("userName").equalTo(userName);
 
-    database.ref("/users").push({
-        firstName: firstName,
-        lastName: lastName,
-        winRecord: winRecord,
-        userName: userName,
-        lossRecord: lossRecord
-    });
-
-
-    // if (database.ref("/users").orderByChild('userName').equalTo(userName)) {
-
-    //     console.log("This value is in the database");
-
-    //     database.ref("/users").on("value", function (snapshot) {
-
-    //     Searches database for existing username.
-    //     Source: https://stackoverflow.com/questions/40471284/firebase-search-by-child-value
-
-    //     userName = snapshot.val().userName;
-    //     winRecord = snapshot.val().winRecord;
-    //     lossRecord = snapshot.val().lossRecord;
-    // } else {
-    //     users.push({
-    //         firstName: firstName,
-    //         lastName: lastName,
-    //         winRecord: winRecord,
-    //         userName: userName,
-    //         lossRecord: lossRecord
-    //     });
-    // };
-
-    // console.log(userName);
-    // console.log(winRecord);
-    // console.log(lossRecord);
-
-    database.ref("/users").orderByChild("userName").equalTo(userName).on("value", function (snapshot) {
-
+    // Pulls data based on existing users and go through each child key.
+    // Sets the entire object and username
+    userNameSearch.on("value", function (snapshot) {
         // Search and pull child data
         // Source: https://github.com/firebase/functions-samples/issues/265
-        snapshot.forEach(function(childSnapshot) {
-            var childData = childSnapshot.val();
-            userName = childData.userName;
-            winRecord = childData.winRecord;
-            lossRecord = childData.lossRecord;
-
+        snapshot.forEach(function (childSnapshot) {
+            existingUserObj = childSnapshot.val();
+            existingUserName = existingUserObj.userName;
         });
     });
 
-    $("#username-display").text(userName);
-    $("#record-display").text(winRecord + " - " + lossRecord);
+    // If there is an existing user in the database based on User Name input value.
+    // Set the displayed username and record based on previously pulled object
+    if (existingUserName === userName) {
+
+        $("#username-display").text(existingUserObj.userName);
+        $("#record-display").text(existingUserObj.winRecord + " - " + existingUserObj.lossRecord);
+
+    // If not in the database, push user to the database, set the global variables, and set displays text.
+    } else {
+
+        database.ref("/users").push({
+            firstName: firstName,
+            lastName: lastName,
+            winRecord: winRecord,
+            userName: userName,
+            lossRecord: lossRecord
+        });
+
+        userNameSearch.on("value", function (snapshot) {
+            snapshot.forEach(function (childSnapshot) {
+                existingUserObj = childSnapshot.val();
+                existingUserName = existingUserObj.userName;
+            });
+
+            $("#username-display").text(existingUserObj.userName);
+            $("#record-display").text(existingUserObj.winRecord + " - " + existingUserObj.lossRecord);
+        });
+
+    };
+
+    $(".form-control").val("");
+
+    database.ref("/player1").on("value", function(snap) {
+        playerOne = snap.val();
+        console.log(playerOne);
+    });
+
+    database.ref("/player2").on("value", function(snap) {
+        playerTwo = snap.val();
+        console.log(playerTwo);
+    });
+
+    if (playerOne === null) {
+        database.ref("/player1").set(existingUserObj);
+    } else if (playerTwo === null) {
+        database.ref("/player2").set(existingUserObj);
+    } else {
+        console.log("Players 1 & 2 are set.");
+    }
+
 });
