c3cfa5e8efe781a7eb80c2ae179f0a59edc9dbfe
TerrenceMM2 <terrencemm2@gmail.com>
2019-06-06 23:10:26 -0500

    switches out jQuery for Vanilla JS

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,152 +1,180 @@
-$(document).ready(function () {
+// Your web app's Firebase configuration
+var config = {
+    apiKey: "AIzaSyBLw--mS2bA2RRZXOi9nbadZavdBA1JaUA",
+    authDomain: "coursework7-rps.firebaseapp.com",
+    databaseURL: "https://coursework7-rps.firebaseio.com",
+    projectId: "coursework7-rps",
+    storageBucket: "",
+    messagingSenderId: "288062732004",
+    appId: "1:288062732004:web:832a923f7ff1acac"
+};
+// Initialize Firebase
+firebase.initializeApp(config)
 
-    // Your web app's Firebase configuration
-    var config = {
-        apiKey: "AIzaSyBLw--mS2bA2RRZXOi9nbadZavdBA1JaUA",
-        authDomain: "coursework7-rps.firebaseapp.com",
-        databaseURL: "https://coursework7-rps.firebaseio.com",
-        projectId: "coursework7-rps",
-        storageBucket: "",
-        messagingSenderId: "288062732004",
-        appId: "1:288062732004:web:832a923f7ff1acac"
+var database = firebase.database();
+
+// Initial Values
+var firstName = "";
+var lastName = "";
+var userName = "";
+var winRecord = 0;
+var lossRecord = 0;
+
+// Round Initial Values
+var roundCount = 0;
+var winCount = 0;
+var lossCount = 0;
+
+var selectedUserName = "";
+var selectedUserObj;
+
+var playerOne;
+var playerTwo;
+var searchPlayerOne = "";
+var searchPlayerTwo = "";
+
+
+document.getElementById("new-game").addEventListener("click", function (event) {
+    event.preventDefault();
+
+    database.ref("/player1").set({
+        userName: "Ready Player 1"
+    });
+    database.ref("/player2").set({
+        userName: "Ready Player 2"
+    });
+
+    document.getElementById("player-selection").style.display = "block";
+    document.getElementById("new-game").style.display = "none";
+});
+
+
+// database.ref().on("value", function (snapshot) {
+
+//     if (snapshot.child("username").exists()) {
+//         // Set the variables for highBidder/highPrice equal to the stored values in firebase.
+//         userName = snapshot.val().userName;
+//         winRecord = snapshot.val().winRecord;
+//         lossRecord = snapshot.val().lossRecord;
+
+//         document.getElementById("username-display").innerHTML = username;
+//         document.getElementById("record-display").innerHTML = winRecord + " - " + lossRecord;
+//     }
+// });
+
+database.ref("/player1").on("value", function (snap) {
+    playerOne = snap.val();
+    searchPlayerOne = playerOne.userName;
+    document.getElementById("p1-username-display").innerHTML = playerOne.userName;
+    document.getElementById("p1-record-display").innerHTML = playerOne.winRecord + " - " + playerOne.lossRecord;
+    if (searchPlayerOne !== "Ready Player 1") {
+        console.log(1);
+        document.getElementById("new-game").style.display = "none";
+        document.getElementById("player-selection").style.display = "block";
     };
-    // Initialize Firebase
-    firebase.initializeApp(config)
+});
 
-    var database = firebase.database();
+database.ref("/player2").on("value", function (snap) {
+    playerTwo = snap.val();
+    searchPlayerTwo = playerTwo.userName;
+    document.getElementById("p2-username-display").innerHTML = playerTwo.userName;
+    document.getElementById("p2-record-display").innerHTML = playerTwo.winRecord + " - " + playerTwo.lossRecord;
+});
 
-    // Initial Values
-    var firstName = "";
-    var lastName = "";
-    var userName = "";
-    var winRecord = 0;
-    var lossRecord = 0;
+document.getElementById("set-player").addEventListener("click", function (event) {
 
-    // Round Initial Values
-    var roundCount = 0;
-    var winCount = 0;
-    var lossCount = 0;
+    event.preventDefault();
 
-    var selectedUserName = "";
-    var selectedUserObj;
+    firstName = document.getElementById("first-name-input").value
+    lastName = document.getElementById("last-name-input").value
+    userName = document.getElementById("username-input").value
 
-    var playerOne;
-    var playerTwo;
-    var searchPlayerOne = "";
-    var searchPlayerTwo = "";
+    // Sets the search reference location if an existing username is found
+    var userNameSearch = database.ref("/users").orderByChild("userName").equalTo(userName);
 
-    $("#new-game").on("click", function (event) {
-        event.preventDefault();
-
-        database.ref("/player1").set({
-            userName: "Ready Player 1"
+    // Pulls data based on existing users and go through each child key.
+    // Sets the entire object and username
+    userNameSearch.on("value", function (snapshot) {
+        // Search and pull child data
+        // Source: https://github.com/firebase/functions-samples/issues/265
+        snapshot.forEach(function (childSnapshot) {
+            selectedUserObj = childSnapshot.val();
+            selectedUserName = selectedUserObj.userName;
         });
-        database.ref("/player2").set({
-            userName: "Ready Player 2"
+    });
+
+    // If not in the database, push user to the database, set the global variables, and set displays text.
+    // Set the displayed username and record based on previously pulled object
+    if (selectedUserName !== userName) {
+
+        database.ref("/users").push({
+            firstName: firstName,
+            lastName: lastName,
+            winRecord: winRecord,
+            userName: userName,
+            lossRecord: lossRecord
         });
 
-        $("#player-selection").show();
-        $("#new-game").hide();
-    });
-
-    database.ref().on("value", function (snapshot) {
-
-        if (snapshot.child("username").exists()) {
-            // Set the variables for highBidder/highPrice equal to the stored values in firebase.
-            userName = snapshot.val().userName;
-            winRecord = snapshot.val().winRecord;
-            lossRecord = snapshot.val().lossRecord;
-
-            // Change the HTML to reflect the stored values
-            $("#username-display").text(userName);
-            $("#record-display").text(winRecord + " - " + lossRecord);
-        }
-    });
-
-    database.ref("/player1").on("value", function (snap) {
-        playerOne = snap.val();
-        searchPlayerOne = playerOne.userName;
-        $("#p1-username-display").text(playerOne.userName);
-        $("#p1-record-display").text(playerOne.winRecord + " - " + playerOne.lossRecord);
-    });
-
-    database.ref("/player2").on("value", function (snap) {
-        playerTwo = snap.val();
-        searchPlayerTwo = playerTwo.userName;
-        $("#p2-username-display").text(playerTwo.userName);
-        $("#p2-record-display").text(playerTwo.winRecord + " - " + playerTwo.lossRecord);
-    });
-
-    $("#set-player").on("click", function (event) {
-
-        event.preventDefault();
-
-        firstName = $("#first-name-input").val().trim();
-        lastName = $("#last-name-input").val().trim();
-        userName = $("#username-input").val().trim();
-
-        // Sets the search reference location if an existing username is found
-        var userNameSearch = database.ref("/users").orderByChild("userName").equalTo(userName);
-
-        // Pulls data based on existing users and go through each child key.
-        // Sets the entire object and username
         userNameSearch.on("value", function (snapshot) {
-            // Search and pull child data
-            // Source: https://github.com/firebase/functions-samples/issues/265
             snapshot.forEach(function (childSnapshot) {
                 selectedUserObj = childSnapshot.val();
                 selectedUserName = selectedUserObj.userName;
             });
         });
+    };
 
-        // If not in the database, push user to the database, set the global variables, and set displays text.
-        // Set the displayed username and record based on previously pulled object
-        if (selectedUserName !== userName) {
+    if (searchPlayerOne === "Ready Player 1") {
+        database.ref("/player1").set(selectedUserObj);
+    } else if (searchPlayerOne === userName) {
+        console.log("Please choose another user");
+    } else if (searchPlayerTwo === "Ready Player 2") {
+        database.ref("/player2").set(selectedUserObj);
+    } else {
+        console.log("Player 1 & 2 are set.")
+    }
+    
 
-            database.ref("/users").push({
-                firstName: firstName,
-                lastName: lastName,
-                winRecord: winRecord,
-                userName: userName,
-                lossRecord: lossRecord
-            });
+    document.getElementById("current-players").style.display = "block";
+    document.getElementById("player-selection").style.display = "none";
 
-            userNameSearch.on("value", function (snapshot) {
-                snapshot.forEach(function (childSnapshot) {
-                    selectedUserObj = childSnapshot.val();
-                    selectedUserName = selectedUserObj.userName;
-                });
-            });
-        };
+    document.getElementsByClassName("form-control").innerHTML = "";
 
-        console.log(searchPlayerOne);
-        console.log(searchPlayerTwo)
+});
 
-        if (searchPlayerOne === "Ready Player 1") {
-            database.ref("/player1").set(selectedUserObj);
-        } else if (searchPlayerOne === userName) {
-            console.log("Please choose another user");
-        } else if (searchPlayerTwo === "Ready Player 2") {
-            database.ref("/player2").set(selectedUserObj);
-        } else {
-            console.log("Player 1 & 2 are set.")
-        }
+// if (searchPlayerOne !== "Ready Player 1") {
+//     console.log(1);
+//     document.getElementById("new-game").style.display = "none";
+//     document.getElementById("player-selection").style.display = "block";
+// } else if (searchPlayerOne === userName) {
+//     console.log(3);
+//     console.log("Please choose another user");
+// } else if (searchPlayerTwo === "Ready Player 2") {
+//     console.log(4);
+//     database.ref("/player2").set(selectedUserObj);
+// } else {
+//     console.log(5);
+//     console.log("Player 1 & 2 are set.")
+// }
 
-        $("#current-players").show();
-        $("#player-selection").hide();
 
-        $(".form-control").val("");
 
+
+
+
+
+
+// Temp Reset Button
+document.getElementById("reset-game").addEventListener("click", function (event) {
+    event.preventDefault();
+
+    database.ref("/player1").set({
+        userName: "Ready Player 1"
+    });
+    database.ref("/player2").set({
+        userName: "Ready Player 2"
     });
 
-
-if (searchPlayerOne !== "Ready Player 1" && searchPlayerTwo !== "Ready Player 2") {
-    $("#new-game").hide();
-    $("#player-selection").hide();
-    $("#current-players").show();
-} else if (searchPlayerOne !== "Ready Player 1") {
-    $("#new-game").hide();
-    $("#player-selection").show();
-};
-
+    document.getElementById("player-selection").style.display = "none";
+    document.getElementById("current-players").style.display = "none";
+    document.getElementById("new-game").style.display = "block";
 });
